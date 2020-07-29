f3e52b8c068d2d0e42907c86b28cfde26fb118cb
Antolina Gonzalez <antolina487@gmail.com>
2020-07-29 08:50:02 -0300

    main

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path');
+mongoose.Promise = global.Promise;
+
+const app = express()
+
+const db = require('./config/db').database;
+
+//database connection 
+mongoose.connect(db,{
+         useNewUrlParser:true,
+         useCreateIndex:true,
+         useUnifiedTopology:true
+    })
+    .then(() => {
+        console.log('Database success')
+    })
+    .catch((err) => {
+        console.log('Unable to connect', err)
+    });
+//definiendo el puerto 
+const port = process.env.PORT || 5000;
+
+//Middleware cors
+app.use(cors());
+
+// BpodyParser middleware
+
+app.use(bodyParser.json());
+
+// mongoose.connect(url, {
+//     useNewUrlParser:true,
+//     useCreateIndex:true,
+//     useUnifiedTopology:true
+// })
+// const con = mongoose.connection
+
+// con.on('open', () => {
+//     console.log('connected...')
+// })
+// Public directory
+/*
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+*/
+const postRoutes = require('./routes/apis/post');
+app.use('/post/api', postRoutes)
+
+app.listen(port, () => {
+    console.log('servidor listo')
+});
