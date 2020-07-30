bd57a809d006c9e1a959569c3bd1a4b1562b8692
Antolina Gonzalez <antolina487@gmail.com>
2020-07-29 23:48:01 -0300

    ajax request

--- public/main.js
+++ public/main.js
@@ -1,7 +1,21 @@
+
+var traerDatos = (element) =>{
+    console.log("Anto es una nena ninda")
+    var id = element._id;
+    var title = element.title;
+    var text = element.text;
+    console.log(element._id)
+    
+        //var preProcesado = datos.json[1].title
+    window.location = "/edit.html?var="+id+"&title="+title+"&text="+text;
+   
+    //return datos;
+}
+
 $(function(){
+
     //get all the posts
     $('#getPost').ready(function(){
-        
         $.ajax({ url: '/api',
                 context: document.body,
                 success: function(posts){
@@ -11,21 +25,22 @@ $(function(){
                     posts.forEach(element => {
                         cont = cont - 1;
                         posteos.append(`
-                        <p style="display: none;" id="idPost" >${element._id}</p>
                         <div class="bordes-Post" >
+                            <p style="display: none;" class="idPost" >${element._id}</p>
                             <div class="Descripcion-Post">
                                 <div>
-                                    <h3 >Post ${cont}</h1>
+                                    <h3>Post ${cont}</h1>
                                 </div>
                                 <div>
                                     <h3>${element.updatedAt}<h3>
                                 </div>
                             </div>
-                            <h2 class="color" >${element.title}</h1>
-                            <h2>${element.text}</h2>
+                            <h2 class="color title" >${element.title}</h1>
+                            <h2 class="text">${element.text}</h2>
                             <div class="acciones">
-                                <button class="editPost" ><i class="fas fa-trash-alt"></i></button>
-                                <button class="deletePost" ><i class="fas fa-edit"></i></button>
+                                <a href="#" onclick="traerDatos()" ><i class="fas fa-plus"></i>Editar</a>
+                                <button class="deletePost"><i class="fas fa-trash-alt"></i></button>
+                                <button class="editPost" ><i class="fas fa-edit"></i></button>
                             </div>
                         </div>
                         `
@@ -75,15 +90,87 @@ $(function(){
                 success: function(response){
                     console.log(response);
                     newTitle.val('');
+                    newText.val('');
+                    $('#buttonPost').click();
+                }
+
+        });
+    });
+    $('#editPost').on('submit', function(e){
+        e.preventDefault();
+        let newTitle = $('#newTitle');
+        let newText = $('#newText');
+        $.ajax({ url: '/api',
+                method : 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify({
+                    title : newTitle.val(),
+                    text : newText.val()
+                }),
+                success: function(response){
+                    console.log(response);
+                    newTitle.val('');
+                    newText.val('');
                     $('#buttonPost').click();
                 }
 
         });
     });
 
-    //UPDATE A POST
-    $('div').on('click', '.editPost',function(){
-        console.log('anda');
-    });
+    //UPDATE A POST --sin terminar--
+    // $('.editPost').on('click', function(e){
+    //     e.preventDefault();
+    //     let newTitle = $('#newTitle');
+    //     let newText = $('#newText');
+    //     $.ajax({ url: '/api',
+    //             method : 'POST',
+    //             contentType: 'application/json',
+    //             data: JSON.stringify({
+    //                 title : newTitle.val(),
+    //                 text : newText.val()
+    //             }),
+    //             success: function(response){
+    //                 console.log(response);
+                    
+    //                 $('#buttonPost').click();
+    //             }
 
+    //     });
+    // });
+    $('#getPost').on('click', '.editPost', function(){
+       let posteo = $(this).closest('.bordes-Post');
+        let id = posteo.find('.idPost').text();
+         let title = posteo.find('.title').text();
+         let text = posteo.find('.text').text()
+                 
+         $.ajax({ url: '/api/' + id,
+                method: 'GET',
+                 success: (function(element){
+                    traerDatos(element);
+                 })
+         });
+     });
+
+    $('#getPost').on('click','.deletePost', function(){
+        let posteo = $(this).closest('.bordes-Post');
+        let id = posteo.find('.idPost').text();
+        $.ajax({ url: '/api/' + id,
+                method: 'DELETE',
+                success: (function(){
+                    // ACTUALIZAR LA PAGINA tiene errores
+                $('.loading').load("blog.html");
+                })
+        });
+    });
+    $('#deleteall').on('click' ,function(){
+        
+        $.ajax({ url: '/api/',
+                method: 'DELETE',
+                success: (function(){
+                    // ACTUALIZAR LA PAGINA tiene errores
+                $('.loading').load("blog.html");
+                })
+        });
+    })
 });
+
